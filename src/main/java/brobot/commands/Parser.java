d5736867a9f6dc034b1a95b72991524a535e7113
Matthew Kan (NUS Student) <e0795274@u.nus.edu>
2025-08-26 20:02:28 +0800

    Finish Parser

--- src/main/java/brobot/commands/Parser.java
+++ src/main/java/brobot/commands/Parser.java
@@ -1,14 +1,29 @@
 package brobot.commands;
 
+import brobot.brobotexceptions.BrobotCommandFormatException;
+import brobot.brobotexceptions.NoSuchCommandNameException;
+
 final class Parser {
     private Parser() {
 
     }
 
-    // TODO: Here, handle Commands by handling exceptions. Generic Exceptions and BroBotCommandExceptions.
-    public static Command parseCommand (final String commandName, final String... commandTokens) {
-        try {
-            re
-        }
+    public static Command parseCommand (final String commandName, final String... commandTokens) throws BrobotCommandFormatException {
+        final String cleanedCommandName = commandName.strip().toLowerCase();
+        return switch (cleanedCommandName) {
+            case "todo", "event", "deadline" -> AddTaskCommand.makeCommand(cleanedCommandName, commandTokens);
+
+            case "bye" -> ByeCommand.makeCommand(cleanedCommandName, commandTokens);
+
+            case "list" -> ListCommand.makeCommand(cleanedCommandName, commandTokens);
+
+            case "mark" -> MarkCommand.makeCommand(cleanedCommandName, commandTokens);
+
+            case "unmark" -> UnmarkCommand.makeCommand(commandName, commandTokens);
+
+            case "delete" -> DeleteCommand.makeCommand(commandName, commandTokens);
+
+            default -> throw NoSuchCommandNameException.newInstancefromCommandName(commandName);
+        };
     }
 }
