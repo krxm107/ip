2ed920f9581f51d9b8dce53380d7035ade279ed0
Matthew Kan (NUS Student) <e0795274@u.nus.edu>
2025-08-26 20:18:29 +0800

    Finish Parser

--- src/main/java/brobot/commands/Parser.java
+++ src/main/java/brobot/commands/Parser.java
@@ -1,6 +1,7 @@
 package brobot.commands;
 
 import brobot.brobotexceptions.BrobotCommandFormatException;
+import brobot.brobotexceptions.EmptyCommandException;
 import brobot.brobotexceptions.NoSuchCommandNameException;
 
 final class Parser {
@@ -8,7 +9,22 @@ final class Parser {
 
     }
 
-    public static Command parseCommand (final String commandName, final String... commandTokens) throws BrobotCommandFormatException {
+    public static Command parseCommand (final String commandInput) throws BrobotCommandFormatException {
+        if (commandInput == null || commandInput.isEmpty()) {
+            throw new EmptyCommandException();
+        }
+
+        final String[] parts = commandInput.strip().split("\\s+", -1);
+
+        final String[] tokens = new String[parts.length - 1];
+        for (int i = 0; i < tokens.length; i++) {
+            tokens[i] = parts[i + 1];
+        }
+
+        return Parser.parseCommand(parts[0], tokens);
+    }
+
+    private static Command parseCommand (final String commandName, final String... commandTokens) throws BrobotCommandFormatException {
         final String cleanedCommandName = commandName.strip().toLowerCase();
         return switch (cleanedCommandName) {
             case "todo", "event", "deadline" -> AddTaskCommand.makeCommand(cleanedCommandName, commandTokens);
