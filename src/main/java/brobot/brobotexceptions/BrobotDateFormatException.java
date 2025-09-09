491e113d9a5f2bb8f6fc087696c5e66daff8f5df
Matthew Kan (NUS Student) <e0795274@u.nus.edu>
2025-09-09 18:33:15 +0800

    Add support for more natural date formats.
    15 September 2025 is now allowed.

--- src/main/java/brobot/brobotexceptions/BrobotDateFormatException.java
+++ src/main/java/brobot/brobotexceptions/BrobotDateFormatException.java
@@ -1,10 +1,20 @@
 package brobot.brobotexceptions;
 
 import brobot.BroBot;
-import brobot.datesandtimes.BrobotDate;
 
 /**
- * This exception is thrown when the user enters a date that is not in the 'dd MMM yyyy' format.
+ * This exception is thrown when the user enters a date that is not in the correct format.
+ *
+ *     Examples of correct date formats:
+ *         "01 Sep 2025"
+ *         "01 September 2025"
+ *         "1 Sep 2025"
+ *         "1 September 2025"
+ *
+       Notes:
+ *          - All month names must be in English.
+ *          - Upper/lowercase does not matter ("Sep", "SEP", "september", etc.).
+ *          - The leading zero for the day ("01") is optional.
  */
 public final class BrobotDateFormatException extends BrobotCommandFormatException {
     private BrobotDateFormatException(final String mainMessage) {
@@ -20,19 +30,27 @@ public final class BrobotDateFormatException extends BrobotCommandFormatExceptio
      */
     public static BrobotDateFormatException newInstance(final String invalidDate) {
         final String line1 = String.format(BroBot.ENGLISH_LANGUAGE, "The date you entered, '%s', is in the wrong format.",
-                                            invalidDate);
+                                                    invalidDate);
 
-        final String line2 = String.format(BroBot.ENGLISH_LANGUAGE, "All dates must be in the '%s' format, explained as follows:",
-                                            BrobotDate.DATE_FORMAT);
+        final String line2 = "";
 
-        final String line3 = "dd: Day must be a day in the month, with exactly 2 digits, starting with 0 if necessary.";
+        final String line3 = "Examples of dates in the correct format are:";
 
-        final String line4 = "MMM: Month must be the first three letters of the month, "
-                                 + "with the first in uppercase and the last 2 in lowercase.";
+        final String line4 = "    01 Sep 2025";
+        final String line5 = "    1 Sep 2025";
+        final String line6 = "    01 September 2025";
+        final String line7 = "    1 September 2025";
 
+        final String line8 = "";
+        final String line9 = "Notes:";
 
-        final String line5 = "yyyy: Year must have exactly 4 digits.";
+        final String line10 = "    All month names must be in English.";
+        final String line11 = "    Upper/lowercase does not matter (\"Sep\", \"SEP\", \"september\", etc.).";
+        final String line12 = "    The leading zero for the day (\"01\") is optional.";
 
-        return new BrobotDateFormatException(String.join(System.lineSeparator(), "", line1, line2, line3, line4, line5, ""));
+        final String mainMessage = String.join(System.lineSeparator(), line1, line2, line3, line4, line5, line6,
+                                                line7, line8, line9, line10, line11, line12);
+
+        return new BrobotDateFormatException(mainMessage);
     }
 }
