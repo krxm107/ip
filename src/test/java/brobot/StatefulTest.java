ffef3f21c73fc79abab7f915d36ad858d4a44ef8
Matthew Kan (NUS Student) <e0795274@u.nus.edu>
2025-09-11 01:00:21 +0800

    Improve Unit Testing for MassOps

--- src/test/java/brobot/StatefulTest.java
+++ src/test/java/brobot/StatefulTest.java
@@ -7,23 +7,13 @@ import org.junit.jupiter.api.BeforeEach;
  * where each test requires a clean, clear state at the beginning
  * to prevent unwanted side effects and dependencies.
  */
-public interface StatefulTester {
+public interface StatefulTest {
     /**
      * Runs before each test to clear the state and avoid said dependencies.
      * By default, we clear the TaskList singleton so that it's empty and well-behaved.
      */
     @BeforeEach
     default void clearState() {
-        StatefulTester.clearTaskList();
-    }
-
-    /**
-     * Public static helper method to clear the tasklist singleton to help reset the state.
-     * DO NOT HIDE OR OVERRIDE.
-     */
-    static void clearTaskList() {
-        while (!TaskList.getSingleton().isEmpty()) {
-            TaskList.getSingleton().removeFromTaskList(TaskList.getSingleton().getSize());
-        }
+        TaskListHelper.clearTaskList();
     }
 }
